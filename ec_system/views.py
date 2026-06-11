d875022c7b8e74169404359ad643286ad3792023
Saburo Sato <s-satou@example.com>
2026-06-11 11:52:01 +0900

    add main.html 未ログイン時

--- ec_system/views.py
+++ ec_system/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from . import models
 
-# Create your views here.
+
+def index(request):
+    return render(request, "ec_system/main.html")
