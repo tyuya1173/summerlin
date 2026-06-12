52611b9a03afaef70a2e4cb2c6edb97acff7096c
take <take@example.com>
2026-06-12 10:57:11 +0900

    add itemDetail

--- ec_system/forms.py
+++ ec_system/forms.py
@@ -19,3 +19,15 @@ class ItemSearchForm(forms.Form):
         choices = [("all", "すべて")]
         choices += [(str(c.category_id), c.name) for c in Category.objects.all()]
         self.fields["category"].choices = choices
+
+class IteminCartForm(forms.Form):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
+
+    amountForm = forms.ChoiceField(
+        label="数量",
+        choices=[(i, i) for i in range(1, 101)],
+        widget=forms.widgets.Select
+    )
--- ec_system/urls.py
+++ ec_system/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('searchResult/', views.SearchResult.as_view(), name="search_result"),
     path('login/', views.Login.as_view(), name="login"),
+    path('itemDetail/<int:item_id>/', views.Itemdetail.as_view(), name="item_detail")
 ]
